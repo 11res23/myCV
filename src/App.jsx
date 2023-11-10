6984d36a369511aed9747faa936ffbc00684a917
03Res <s22110238@student.unklab.ac.id>
2023-11-10 23:17:41 +0800

    first version template before changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-
-import Home from "./component"
+import Header from "./component/header"
+import Hero from "./component/hyro"
+import About from "./component/about"
+import Facts from "./component/facts"
+import Skill from "./component/skill"
+import Portfolio from "./component/portfolio"
+import Testimonials from "./component/testimonials"
+import Contact from "./component/contact"
+import Footer from "./component/footer"
 function App() {
 
 
   return (
-    <Home />
+    <>
+    <Header />
+    <Hero />
+      <main id="main">
+        <About />
+        <Facts/>
+        <Skill/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+      </main>
+      <Footer/>
+    </>
+    
   )
 }
 
